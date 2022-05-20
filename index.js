a10f60b8a6d6f279d7e80b0cf486356e4a9eac4f
Nargiza Akunova <nargizaakun@gmail.com>
2022-05-20 17:06:49 +0200

    fix: work on fetch and json file

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-fetch("./books.json") //path to the file with json data
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
-
-const fragment = new DocumentFragment();
+const booksEndpoint = "./books.json";
 const selectedBooks = [];
 
-// insert navigation into fragment
-fragment.appendChild(createNav());
-// insert welcome catalog text into fragment
-fragment.appendChild(createWelcomeCatalog());
-// insert books catalog into fragment
-fragment.appendChild(createBooksCatalog());
-// insert footer into fragment
-fragment.appendChild(createFooter());
+fetchBooks().then((items) => renderPage(items));
 
-// insert fragment
-document.body.prepend(fragment);
+function fetchBooks() {
+  return fetch(booksEndpoint).then((response) => {
+    return response.json();
+  });
+}
+
+function renderPage(books) {
+  const fragment = new DocumentFragment();
+
+  // insert navigation into fragment
+  fragment.appendChild(createNav());
+  // insert welcome catalog text into fragment
+  fragment.appendChild(createWelcomeCatalog());
+  // insert books catalog into fragment
+  fragment.appendChild(createBooksCatalog(books));
+  // insert footer into fragment
+  fragment.appendChild(createFooter());
+
+  // insert fragment
+  document.body.prepend(fragment);
+}
 
 function createNav() {
   // create navigation
@@ -99,97 +103,13 @@ function createWelcomeCatalog() {
   return welcomeCatalog;
 }
 
-function createBooksCatalog() {
+function createBooksCatalog(items) {
   // create books catalog
   const books = document.createElement("div");
   books.classList.add("books");
 
   const container = document.createElement("div");
   container.classList.add("container");
-
-  const items = [
-    {
-      author: "Douglas Crockford",
-      imageLink: "./images/book1.jpg",
-      title: "JavaScript: The Good Parts: The Good Parts",
-      price: 30,
-      description:
-        "With JavaScript: The Good Parts, you'll discover a beautiful, elegant, lightweight and highly expressive language that lets you create effective code, whether you're managing object libraries or just trying to get Ajax to run fast. If you develop sites or applications for the Web, this book is an absolute must",
-    },
-    {
-      author: "David Herman",
-      imageLink: "./images/book2.jpg",
-      title:
-        "Effective JavaScript: 68 Specific Ways to Harness the Power of JavaScript",
-      price: 22,
-      description:
-        "Effective JavaScript is organized around 68 proven approaches for writing better JavaScript, backed by concrete examples. You’ll learn how to choose the right programming style for each project, manage unanticipated problems, and work more successfully with every facet of JavaScript programming from data structures to concurrency",
-    },
-    {
-      author: "David Flanagan",
-      imageLink: "./images/book3.jpg",
-      title: "JavaScript: The Definitive Guide",
-      price: 40,
-      description:
-        "This Fifth Edition is completely revised and expanded to cover JavaScript as it is used in today's Web 2.0 applications. This book is both an example-driven programmer's guide and a keep-on-your-desk reference, with new chapters that explain everything you need to know to get the most out of JavaScript",
-    },
-    {
-      author: " Eric Elliott",
-      imageLink: "./images/book4.jpg",
-      title: "Programming JavaScript Applications",
-      price: 19,
-      description:
-        "Take advantage of JavaScript’s power to build robust web-scale or enterprise applications that are easy to extend and maintain. By applying the design patterns outlined in this practical book, experienced JavaScript developers will learn how to write flexible and resilient code that’s easier—yes, easier—to work with as your code base grows.",
-    },
-    {
-      author: "Addy Osmani",
-      imageLink: "./images/book5.jpg",
-      title: "Learning JavaScript Design Patterns",
-      price: 32,
-      description:
-        "With Learning JavaScript Design Patterns, you’ll learn how to write beautiful, structured, and maintainable JavaScript by applying classical and modern design patterns to the language. If you want to keep your code efficient, more manageable, and up-to-date with the latest best practices, this book is for you.",
-    },
-    {
-      author: "Boris Cherny",
-      imageLink: "./images/book6.jpg",
-      title: "Programming TypeScript",
-      price: 28,
-      description:
-        "Any programmer working with a dynamically typed language will tell you how hard it is to scale to more lines of code and more engineers. That’s why Facebook, Google, and Microsoft invented gradual static type layers for their dynamically typed JavaScript and Python code. This practical book shows you how one such type layer, TypeScript, is unique among them: it makes programming fun with its powerful static type system.",
-    },
-    {
-      author: "Alex Banks, Eve Porcello",
-      imageLink: "./images/book7.jpg",
-      title: "Learning React, 2nd Edition",
-      price: 25,
-      description:
-        "If you want to learn how to build efficient React applications, this is your book. Ideal for web developers and software engineers who understand how JavaScript, CSS, and HTML work in the browser, this updated edition provides best practices and patterns for writing modern React code. No prior knowledge of React or functional JavaScript is necessary.",
-    },
-    {
-      author: "Bradley Meck Alex Young and Mike Cantelon",
-      imageLink: "./images/book8.jpg",
-      title: "Node.js in Action",
-      price: 38,
-      description:
-        "Node.js in Action, Second Edition is a thoroughly revised book based on the best-selling first edition. It starts at square one and guides you through all the features, techniques, and concepts you'll need to build production-quality Node applications.",
-    },
-    {
-      author: "Kyle Simpson",
-      imageLink: "./images/book9.jpg",
-      title: "You Don't Know JS Yet: Get Started",
-      price: 26,
-      description:
-        "It seems like there's never been as much widespread desire before for a better way to deeply learn the fundamentals of JavaScript. But with a million blogs, books, and videos out there, just where do you START? Look no further!",
-    },
-    {
-      author: "John Resig and Bear Bibeault",
-      imageLink: "./images/book10.jpg",
-      title: "Secrets of the JavaScript Ninja",
-      price: 33,
-      description:
-        "Secrets of the Javascript Ninja takes you on a journey towards mastering modern JavaScript development in three phases: design, construction, and maintenance. Written for JavaScript developers with intermediate-level skills, this book will give you the knowledge you need to create a cross-browser JavaScript library from the ground up.",
-    },
-  ];
 
   for (let item of items) {
     container.appendChild(createBookElement(item));
@@ -445,6 +365,30 @@ function createSidebarWindow(selectedBooks) {
     quantityNumber.name = "quantity";
     quantityNumber.value = quantity;
     quantityNumber.type = "number";
+    quantityNumber.onchange = () => {
+      if (quantityNumber.value > quantity) {
+        const itemsToAdd = quantityNumber.value - quantity;
+        for (let i = 0; i < itemsToAdd; i++) {
+          selectedBooks.push(book);
+        }
+        renderSidebarWindow();
+      } else if (quantityNumber.value < quantity) {
+        const itemsToRemove = quantity - quantityNumber.value;
+        console.log(itemsToRemove);
+        let itemsRemoved = 0;
+        for (let i = 0; i < selectedBooks.length; i++) {
+          if (selectedBooks[i] === book) {
+            selectedBooks.splice(i, 1);
+            itemsRemoved++;
+            i--;
+          }
+          if (itemsRemoved === itemsToRemove) {
+            break;
+          }
+        }
+        renderSidebarWindow();
+      }
+    };
 
     const stepUpBtn = document.createElement("button");
     stepUpBtn.classList.add("plus");
@@ -479,6 +423,9 @@ function createSidebarWindow(selectedBooks) {
     productItem.appendChild(summaryInfo);
     productItem.appendChild(removeBtn);
     products.appendChild(productItem);
+    closeModalBtnLink.appendChild(closeModalBtnIcon);
+    sidebarWindow.appendChild(closeModalBtnLink);
+    sidebarWindow.appendChild(products);
   }
 
   const checkoutBlock = document.createElement("div");
@@ -498,12 +445,15 @@ function createSidebarWindow(selectedBooks) {
   orderBtn.onclick = () => {
     window.open("./form.html", "_blank");
   };
+  if (price === 0) {
+    orderBtn.disabled = "true";
+    orderBtn.style.backgroundColor = "var(--grey-light)";
+    orderBtn.style.color = "var(--grey-dark)";
+    orderBtn.style.cursor = "default";
+  }
 
-  closeModalBtnLink.appendChild(closeModalBtnIcon);
   checkoutBlock.appendChild(totalPrice);
   checkoutBlock.appendChild(orderBtn);
-  sidebarWindow.appendChild(closeModalBtnLink);
-  sidebarWindow.appendChild(products);
   sidebarWindow.appendChild(checkoutBlock);
 
   return sidebarWindow;
